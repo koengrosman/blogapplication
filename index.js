b1749c5e6ffaf21d8bc53dec422f9972cdbf3d32
Koen Grosman <koengrosman@hotmail.com>
2016-02-16 15:17:07 +0100

    begin of sequalize

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var Sequelize = require('sequelize');
 var app = express();
+
+
+var sequelize = new Sequelize("postgres://" + process.env.POSTGRES_USER + ":@localhost/blogapplication");
+
+var Users = sequelize.define('users', {
+	username: Sequelize.STRING,
+	emailadress: Sequelize.TEXT,
+	password: Sequelize.STRING
+});
+
+sequelize.sync().then(function () {
+	Users.create({
+		username: 'birds are chirpy',
+		emailadress: 'chirp chirp',
+		password: 'chirp chirp'
+	});
+});
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
